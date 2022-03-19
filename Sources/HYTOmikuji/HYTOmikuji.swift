cd224ae2fe2d78bebbf601eb2987ef9ea03eee43
hayata-0 <soccerkozo8814@docomo.ne.jp>
2022-03-19 17:36:48 +0900

    ライブラリを作成

--- Sources/HYTOmikuji/HYTOmikuji.swift
+++ Sources/HYTOmikuji/HYTOmikuji.swift
@@ -1,6 +1,25 @@
-public struct HYTOmikuji {
-    public private(set) var text = "Hello, World!"
+import Foundation
 
-    public init() {
+public enum omikuji: String,CaseIterable {
+    case 大吉
+    case 中吉
+    case 小吉
+    case 吉
+    case 末吉
+    case 凶
+    case 大凶
+}
+
+extension omikuji {
+    public static func drow() -> Self {
+        allCases.randomElement()!
     }
+}
+
+extension omikuji: CustomStringConvertible {
+
+    public var description: String {
+        rawValue
+    }
+
 }
